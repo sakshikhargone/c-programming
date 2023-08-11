df6d5e32e7e47dac260d558a1295e701244de6e1
Sakshi Gupta <sakshi.khargone@gmail.com>
2023-08-11 14:48:13 +0530

    My first program in C

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # c-programming
 Programming in C
+
+[Print Hello](/programs/p1.c)
---
+++ programs/p1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Program to print namaste
+
+int main()
+{
+    printf("Namaste C\n");
+    printf("My first program");
+    return 0;
+}
